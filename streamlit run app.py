126da016cb45778e876e4c9bd7f616352ec30e4f
eslamfouad20384-hub <eslam.fouad20384@gmail.com>
2026-03-22 13:50:16 +0200

    Update streamlit run app.py

--- streamlit run app.py
+++ streamlit run app.py
@@ -6,7 +6,7 @@ import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 st.set_page_config(layout="wide")
-st.title("👑 Crypto Smart Money Scanner ELITE - Target حقيقي")
+st.title("👑 Crypto Smart Money ")
 
 # ==============================
 # إعدادات
